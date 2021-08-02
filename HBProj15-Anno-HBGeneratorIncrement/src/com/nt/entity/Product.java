c0f3954bc2e4b73a98e2a3d5bc0b2c20f4ebe2dd
arvindtl <Arvind@192.168.0.103>
2021-08-02 14:17:01 +0530

    HB Increment Generator

---
+++ HBProj15-Anno-HBGeneratorIncrement/src/com/nt/entity/Product.java
@@ -0,0 +1,30 @@
+package com.nt.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.Data;
+
+@Data
+@Entity
+
+@Table(name = "HB_Product_Increment")
+public class Product {
+	@Id
+	@GenericGenerator(name = "gen11",strategy = "increment")
+	@GeneratedValue(generator = "gen11")
+	private Integer pid;
+	
+	private String pname;
+	private Double qty;
+	private Double price;
+	private String status;
+
+}
---
+++ HBProj15-Anno-HBGeneratorIncrement/src/com/nt/test/AnnoSaveIncrement.java
@@ -0,0 +1,36 @@
+package com.nt.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.nt.entity.Product;
+import com.nt.util.HibernateUtil;
+
+public class AnnoSaveIncrement {
+
+	public static void main(String[] args) {
+		try {
+			//get sessionfactory
+			SessionFactory factory=HibernateUtil.getSessionFactory();
+			//get session
+			Session session=HibernateUtil.getSession();
+			Transaction tx=null;
+			tx=session.beginTransaction();
+			
+			Product product=new Product();
+			product.setPname("arvind");
+			product.setPrice(5000.0);
+			product.setQty(3.0);
+			product.setStatus("success");
+			session.save(product);
+			//System.out.println("save object "+vakl);
+			tx.commit();
+		}catch (Exception e) {
+			e.printStackTrace();
+		
+		}
+		
+	
+	}
+}
